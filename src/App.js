771df639969feebe7dbe8b397c8507ec51a7363e
Adrian <adriancubillos@gmail.com>
2020-04-28 01:59:54 -0400

    Integration:Connecting components with react-redux

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Bugs from './components/Bugs';
 import configureStore from './store/configureStore';
-import StoreContext from './contexts/storeContext';
+import { Provider } from 'react-redux';
 
 const store = configureStore();
 
@@ -12,9 +12,9 @@ function App() {
     // where we specify the object that we want to provide to our component tree
     // with this every component below the provider component will get the store object
     // no matter how deep in the component tree the target component (in this case <Bugs/>) may be
-    <StoreContext.Provider value={store}>
+    <Provider store={store}>
       <Bugs />
-    </StoreContext.Provider>
+    </Provider>
   );
 }
 
--- src/contexts/storeContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-
-// Notice the use of Pascal convention for const name
-// because this will be like a class and not an object
-const StoreContext = createContext();
-
-export default StoreContext;
