f52fc6a7080ff0516c44f98ae0a76000024e7664
Adrian <adriancubillos@gmail.com>
2020-04-28 01:36:14 -0400

    Integration: using react context to provide the store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Bugs from './components/Bugs';
+import configureStore from './store/configureStore';
+import StoreContext from './contexts/storeContext';
+
+const store = configureStore();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // The Provider component has a prop called value
+    // where we specify the object that we want to provide to our component tree
+    // with this every component below the provider component will get the store object
+    // no matter how deep in the component tree the target component (in this case <Bugs/>) may be
+    <StoreContext.Provider value={store}>
+      <Bugs />
+    </StoreContext.Provider>
   );
 }
 
